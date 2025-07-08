0d5ad4e29e4d480536fcf5933b179a5806bd6fdd
narmingasimli <nerminqasimli210@gmail.com>
2025-07-08 07:56:03 +0000

    add and update 10-update_topics.py file

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""Update name, topics"""
+"""Update school name, topics"""
 def update_topics(mongo_collection, name, topics):
-    """Update name, topics"""
+    """Update"""
     mongo_collection.update_many(
         { "name": name },
         { "$set": { "topics": topics } }
     )
-    return doc
